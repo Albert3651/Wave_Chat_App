da6de14c4dd6964784d0e46efaeb58e4dfdcb51d
Kobby Mendes <albert13@oegmail.com>
2024-06-14 06:32:23 +0000

    updated name to our_wave_app_project

--- app.config.js
+++ app.config.js
@@ -2,8 +2,8 @@ import 'dotenv/config';
 
 export default {
   "expo": {
-    "name": "ChatApp",
-    "slug": "ChatApp",
+    "name": "Our_wave_app_project",
+    "slug": "Wave_dime",
     "version": "1.0.0",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
